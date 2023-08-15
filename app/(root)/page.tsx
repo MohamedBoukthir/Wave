d18672d9c66166357e987bbb251d4410d71570ab
MohamedBoukthir <medboukthir7@gmail.com>
2023-08-15 11:04:20 +0100

    fetchWaves Homepage

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,35 @@
-export default function Home() {
+import { fetchWaves } from "@/lib/actions/wave.action";
+import { currentUser } from "@clerk/nextjs";
+
+export default async function Home() {
+  const result = await fetchWaves(1, 30);
+  const user = await currentUser();
+
   return (
     <>
       <h1 className="head-text text-left">Home</h1>
+
+      <section className="mt-9 flex flex-col gap-10">
+        { result.waves.length === 0 ? (
+          <p className="no-result">No Waves Found</p>
+        ) : (
+          <>
+            { result.waves.map((wave) => (
+              <WaveCard
+                key={wave._id}
+                id={wave._id}
+                currentUserId={user?.id}
+                parentId={wave.parentId}
+                content={wave.text}
+                author={wave.author}
+                community={wave.community}
+                createdAt={wave.createdAt}
+                comments={wave.children}
+              />
+            )) }
+          </>
+        )}
+      </section>
     </>
   )
 }
--- lib/actions/wave.action.ts
+++ lib/actions/wave.action.ts
@@ -32,3 +32,32 @@ export async function createWave ({ text, author, communityId, path }: Params) {
         throw new Error (`Error creating wave: ${error.message}`)
     }
 }
+
+export async function fetchWaves(pageNumber = 1, pageSize = 20) {
+    connectToDB();
+
+    // calculate the number of posts to skip
+    const skipAmount = (pageNumber - 1) * pageSize;
+
+    // fetch the top level waves
+    const wavesQuery = Wave.find({ parentId: { $in: [null , undefined]}})
+    .sort({ createdAt: 'desc' })
+    .skip(skipAmount)
+    .limit(pageSize)
+    .populate({ path: 'author', model: User })
+    .populate({
+        path: 'children',
+        populate: { 
+            path: 'author',
+            model: User,
+            select: '_id name parentId image' 
+        }
+    })
+
+    const totalWavesCount = await Wave.countDocuments({ parentId: { $in: [null , undefined]}})
+
+    const waves = await wavesQuery.exec();
+
+    const isNext = totalWavesCount > skipAmount + waves.length;
+    return { waves, isNext }
+}
