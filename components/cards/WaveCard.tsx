3b684ed98b2528d0c7d948e56e8ef334531bae90
MohamedBoukthir <medboukthir7@gmail.com>
2023-08-19 11:02:41 +0100

    Profile

--- components/cards/WaveCard.tsx
+++ components/cards/WaveCard.tsx
@@ -60,7 +60,7 @@ const WaveCard = ({
                         <p className="text-small-regular text-light-2">
                             {content}
                         </p>
-                        <div className="mt-5 flex flex-col gap-3">
+                        <div className={`${isComment && 'mb-10'} mt-5 flex flex-col gap-3`} >
                             <div className="flex gap-3.5">
                                 <Image
                                     src='/assets/heart-gray.svg'
--- components/shared/LeftSidebar.tsx
+++ components/shared/LeftSidebar.tsx
@@ -6,13 +6,13 @@ import Link from 'next/link';
 import Image from 'next/image';
 
 import { sidebarLinks } from '@/constants/constants'
-import { SignOutButton, SignedIn } from '@clerk/nextjs';
-
+import { SignOutButton, SignedIn, useAuth } from '@clerk/nextjs';
 
 function LeftSidebar () {
 
     const router = useRouter();
     const pathname = usePathname();
+    const { userId } = useAuth();
 
     return (
         <section className="custom-scrollbar leftsidebar">
@@ -21,6 +21,8 @@ function LeftSidebar () {
 
                     const isActive = (pathname.includes(link.route) 
                     && link.route.length > 1) || pathname === link.route;
+
+                    if(link.route === '/profile') link.route = `${link.route}/${userId}`
 
                     return (
                         <Link 
